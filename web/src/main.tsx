fcd9dd74ecc00a7a77a2cc5622c84f9dfecbb30c
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-17 14:37:30 +0200

    added neutralino initialization plus focus

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,18 @@
+import { app, events, init, window as W } from "@neutralinojs/lib"
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+try { init() } catch (err) { console.warn('Neutralino.js failed to initialize.', err) }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+events.on('windowClose', () => app.exit());
+
+W.focus()
