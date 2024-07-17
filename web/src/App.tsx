64e4c06dfc4ad32c611ac66db14945bf94d31940
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-17 20:42:52 +0200

    changed logo location

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 import { app, filesystem } from "@neutralinojs/lib"
