5370a35f362062044ae3385d98ec8ba0b7b01229
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-18 08:17:55 +0200

    fixed flashing white screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import { app } from "@neutralinojs/lib"
+import { useEffect, useState } from 'react'
+import { app, window as W } from "@neutralinojs/lib"
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
 const shutdownApp = () => app.exit();
 
+
 export default function App() {
   const [count, setCount] = useState(0)
+
+  // This is part of "Eye protection" feature
+  // By default window is starting with white background (changing HTML background color does not help)
+  // So it causes a flash of white screen when the app is starting
+  // I set app window to be hidden by default and then show it after React is loaded
+  // Dev tools (if enabled) will show up before the main window is shown
+  useEffect(() => {
+    W.show()
+  }, [])
 
   return (
     <>
