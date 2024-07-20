0df88ab73f198ca111b7268e53cf9961b397acc2
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-20 22:44:10 +0200

    Update Home.tsx

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,25 +1,28 @@
 import Centered from "../components/Centered"
 
-type TechnologiesType = { name: string, logo: string, url: string, color?: boolean }
+import WouterLogo from '../assets/wouter.svg'
+
+type TechnologiesType = { name: string, logo: string, url: string, color?: string }
 
 const technologies = [
-  { name: 'Neutralinojs', logo: 'i-simple-icons:neutralinojs', url: 'https://neutralino.js.org/docs/', color: true },
+  { name: 'Neutralinojs', logo: 'i-simple-icons:neutralinojs', url: 'https://neutralino.js.org/docs/', color: '#f89901' },
   { name: 'React', logo: 'i-devicon:react', url: 'https://reactjs.org/' },
-  { name: 'Wouter', logo: 'i-pajamas:severity-unknown', url: 'https://github.com/molefrog/Wouter', color: true },
+  { name: 'Wouter', logo: 'i-pajamas:severity-unknown', url: 'https://github.com/molefrog/Wouter', color: '#f89901' },
   { name: 'TypeScript', logo: 'i-logos:typescript-icon', url: 'https://www.typescriptlang.org/' },
   { name: 'TailwindCSS', logo: 'i-logos:tailwindcss-icon', url: 'https://tailwindcss.com/docs/installation' },
   { name: 'UnoCSS', logo: 'i-vscode-icons:file-type-unocss', url: 'https://unocss.dev/' },
   { name: 'daisyUI', logo: 'i-logos:daisyui', url: 'https://daisyui.com/' },
   { name: 'Vite', logo: 'i-logos:vitejs', url: 'https://vitejs.dev/guide/why.html' },
   { name: 'BUN', logo: 'i-logos:bun', url: 'https://bun.sh/' },
-  { name: 'Iconify', logo: 'i-line-md:iconify2', url: 'https://iconify.design/' },
+  { name: 'Iconify', logo: WouterLogo, url: 'https://iconify.design/' },
 ]
 
 const techDiv = (tech: TechnologiesType) => (
   <div key={tech.name} className='flex flex-row items-center m-4 aspect-square rounded-2xl h-32'>
     {/* two divs in column. first take space available, second is at bottom*/}
     <a href={tech.url} className={`flex flex-col items-center w-full h-full rounded-2xl`}>
-      <span className={`flex-grow w-full h-full text-5xl text-white ${tech.logo} ${tech.color && 'bg-red'}`}></span>
+      {/* need to fix that colors below */}
+      <span className={`flex-grow w-full h-full text-5xl text-white ${tech.logo} ${tech.color && `bg-primary`}`}></span>
       <span className='text-lg text-white self-center'>{tech.name}</span>
     </a>
   </div>
