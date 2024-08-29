019fd90dbd2df490727becb07173ea46ac22a054
Avaray <1459553+Avaray@users.noreply.github.com>
2024-08-29 22:14:18 +0200

    Update Header.tsx

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -7,13 +7,13 @@ export default function Header() {
   return (
     <div className="w-full navbar">
       <div className="navbar-start">
-        <Link className='btn btn-lg btn-ghost' href="/"><span className='i-simple-icons:neutralinojs'></span>Neutralino</Link>
+        <Link className='btn btn-lg btn-ghost' href="/"><span className='i-simple-icons:neutralinojs' />Neutralino</Link>
       </div>
       <div className="navbar-end gap-x-2">
         <Link className='btn btn-ghost' href="/">Home</Link>
         <Link className='btn btn-ghost' href="/info">Info</Link>
         <Link className='btn btn-ghost' href="/settings">Settings</Link>
-        <Link className='btn btn-secondary' href='#' onClick={shutdownApp}><span className='i-material-symbols-light:close-rounded'></span></Link>
+        <Link className='btn btn-secondary' href='#' onClick={shutdownApp}><span className='i-material-symbols-light:close-rounded' /></Link>
       </div>
     </div>
   );
