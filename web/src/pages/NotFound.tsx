af6bdc9484106c98308f7dfabd889ead63be732f
Avaray <1459553+Avaray@users.noreply.github.com>
2024-10-29 19:41:07 +0100

    updated 404 page

--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -1,7 +1,10 @@
+import { Centered } from "../components/Centered"
+
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <p className="text-lg">404 - Page not found</p>
-    </div>
+    <Centered>
+      <div className="i-game-icons:bookmarklet op-80 h-20vh w-20vh mt-[-10vh]" />
+      <p className="sm:text-md md:text-xl lg:text-3xl select-none">Page not found</p>
+    </Centered>
   )
 }
